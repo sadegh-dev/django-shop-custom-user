f8dce5af99122055e712d0c836d645c3b0e55188
charlie <sadegh.rez@gmail.com>
2021-05-25 13:36:54 +0430

    AuthUserModel in settings

--- djshop/djshop/settings.py
+++ djshop/djshop/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Develope
+
+AUTH_USER_MODEL = 'accounts.User'
+
